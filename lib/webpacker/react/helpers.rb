707d6b77d3f531acf7463208abf52d7a7ad8b593
Renaud Chaput <renchap@gmail.com>
2017-01-11 21:27:50 +0100

    Make props optionals for react_component()

--- lib/webpacker/react/helpers.rb
+++ lib/webpacker/react/helpers.rb
@@ -1,7 +1,7 @@
 module Webpacker
   module React
     module Helpers
-      def react_component(class_name, props, &block)
+      def react_component(class_name, props = {}, &block)
         tag('div', data: { 'react-class' => class_name, 'react-props' => props.to_json })
       end
     end

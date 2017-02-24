b617c812bdfbdf1120fc1dbfaad6b58010a6f1ce
Artur Roszczyk <artur.roszczyk@gmail.com>
2017-02-24 22:11:20 +0100

    Support component mounting and unmounting in Turbolinks5, Turbolinks 2.4+, Pjax and native
    React components will be mounted and unmounted on events specific for the technologies listed above.
    This code is copied with modifications from https://github.com/reactjs/react-rails with modifications.

--- javascript/webpacker_react-npm-module/src/index.js
+++ javascript/webpacker_react-npm-module/src/index.js
@@ -9,9 +9,88 @@ const WebpackerReact = {
   registeredComponents: {},
   wrapForHMR: null,
 
-  render(node, component) {
-    const propsJson = node.getAttribute(PROPS_ATTRIBUTE_NAME)
-    const props = propsJson && JSON.parse(propsJson)
+  ujs: {
+    handleEvent(eventName, callback) {
+      // jQuery is optional. Use it to support legacy browsers.
+      var $ = (typeof window.jQuery !== 'undefined') && window.jQuery;
+
+      if ($) {
+        $(document).on(eventName, callback);
+      } else {
+        document.addEventListener(eventName, callback);
+      }
+    },
+
+    setup: function (onMount, onUnmount) {
+      var $ = (typeof window.jQuery !== 'undefined') && window.jQuery;
+      // Detect which kind of events to set up:
+      if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
+        if (typeof Turbolinks.EVENTS !== 'undefined') {
+          // Turbolinks.EVENTS is in classic version 2.4.0+
+          this.turbolinksClassic(onMount, onUnmount);
+        } else if (typeof Turbolinks.controller !== "undefined") {
+          // Turbolinks.controller is in version 5+
+          this.turbolinks5(onMount, onUnmount);
+        } else {
+          // ReactRailsUJS.TurbolinksClassicDeprecated.setup();
+          this.turbolinksClassicDeprecated(onMount, onUnmount);
+        }
+      } else if ($ && typeof $.pjax === 'function') {
+        this.pjax(onMount, onUnmount);
+      } else {
+        this.native(onMount);
+      }
+    },
+
+    turbolinks5: function (onMount, onUnmount) {
+      this.handleEvent('turbolinks:load', onMount);
+      this.handleEvent('turbolinks:before-render', onUnmount);
+    },
+
+    turbolinksClassic: function (onMount, onUnmount) {
+      this.handleEvent(Turbolinks.EVENTS.CHANGE, onMount);
+      this.handleEvent(Turbolinks.EVENTS.BEFORE_UNLOAD, onUnmount);
+    },
+
+    turbolinksClassicDeprecated: function (onMount, onUnmount) {
+      // Before Turbolinks 2.4.0, Turbolinks didn't
+      // have named events and didn't have a before-unload event.
+      // Also, it didn't work with the Turbolinks cache, see
+      // https://github.com/reactjs/react-rails/issues/87
+      Turbolinks.pagesCached(0);
+      this.handleEvent('page:change', onMount);
+      this.handleEvent('page:receive', onUnmount);
+    },
+
+    pjax: function (onMount, onUnmount) {
+      this.handleEvent('ready', onMount);
+      this.handleEvent('pjax:end', onMount);
+      this.handleEvent('pjax:beforeReplace', onUnmount);
+    },
+
+    native: function (onMount) {
+      var $ = (typeof window.jQuery !== 'undefined') && window.jQuery;
+
+      if ($) {
+        $(function () {
+          onMount()
+        });
+      } else if ('addEventListener' in window) {
+        document.addEventListener('DOMContentLoaded', function () {
+          onMount()
+        });
+      } else {
+        // add support to IE8 without jQuery
+        window.attachEvent('onload', function () {
+          onMount()
+        });
+      }
+    }
+  },
+
+  render: function (node, component) {
+    var propsJson = node.getAttribute(PROPS_ATTRIBUTE_NAME)
+    var props = propsJson && JSON.parse(propsJson)
 
     let reactElement = React.createElement(component, props)
     if (this.wrapForHMR) {
@@ -22,6 +101,7 @@ const WebpackerReact = {
 
   renderOnHMR(component) {
     const name = component.name
+
     this.registeredComponents[name] = component
 
     if (!this.wrapForHMR) {
@@ -52,29 +132,40 @@ const WebpackerReact = {
     return true
   },
 
-  addEventEventhandlers() {
-    const registeredComponents = this.registeredComponents
+  mountComponents() {
+    var registeredComponents = this.registeredComponents
+    var toMount = document.querySelectorAll('[' + CLASS_ATTRIBUTE_NAME + ']')
 
-    if (this.eventsRegistered === true) {
-      console.warn('webpacker-react: events have already been registered')
+    for (var i = 0; i < toMount.length; ++i) {
+      var node = toMount[i]
+      var className = node.getAttribute(CLASS_ATTRIBUTE_NAME)
+      var component = registeredComponents[className]
+
+      if (component) {
+        this.render(node, component)
+      } else {
+        console.error('webpacker-react: cant render a component that has not been registered: ' + className)
+      }
+    }
+  },
+
+  unmountComponents() {
+    var mounted = document.querySelectorAll('[' + CLASS_ATTRIBUTE_NAME + ']')
+
+    for (var i = 0; i < mounted.length; ++i) {
+      var node = mounted[i]
+
+      ReactDOM.unmountComponentAtNode(node);
+    }
+  },
+
+  addEventEventhandlers() {
+    if (this.eventsRegistered == true) {
+      console.warn("webpacker-react: events have already been registered")
       return false
     }
 
-    document.addEventListener('DOMContentLoaded', () => {
-      const toMount = document.querySelectorAll(`[${CLASS_ATTRIBUTE_NAME}]`)
-
-      for (let i = 0; i < toMount.length; i += 1) {
-        const node = toMount[i]
-        const className = node.getAttribute(CLASS_ATTRIBUTE_NAME)
-        const component = registeredComponents[className]
-
-        if (component) {
-          this.render(node, component)
-        } else {
-          console.error(`webpacker-react: cant render a component that has not been registered: ${className}`)
-        }
-      }
-    })
+    this.ujs.setup(this.mountComponents.bind(this), this.unmountComponents.bind(this));
 
     this.eventsRegistered = true
     return true

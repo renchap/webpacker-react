cf9ce236aed194edb0c095122b7ef35c18ec26e2
Artur Roszczyk <artur.roszczyk@gmail.com>
2017-02-26 20:31:55 +0100

    Bring back legacy WebpackerReact.register method with deprecation warning.
    Show an error, when a functional component is passed

--- javascript/webpacker_react-npm-module/src/index.js
+++ javascript/webpacker_react-npm-module/src/index.js
@@ -42,6 +42,17 @@ const WebpackerReact = {
     this.wrapForHMR = wrapForHMR
   },
 
+  register(component) {
+    console.warn('webpacker-react: WebpackerReact.register(component) has been deprecated, because it can not support functional components. It will be removed in future versions. Use WebpackerReact.setup({Component, ...}) instead.')
+
+    const name = component.name
+    if (name) {
+      this.setup({ [name]: component })
+    } else {
+      console.error('webpacker-react: WebpackerReact.register(component) does not support functional components. Use WebpackerReact.setup({Component, ...) instead')
+    }
+  },
+
   registerComponents(components) {
     const collisions = _.intersection(_.keys(this.registeredComponents), _.keys(components))
     if (collisions.length > 0) {

f9b35097899e5bf34de7563663219c64a70adbcc
Artur Roszczyk <artur.roszczyk@gmail.com>
2017-02-26 13:16:22 +0100

    Fix most of the ESLint issues

--- javascript/webpacker_react-npm-module/src/index.js
+++ javascript/webpacker_react-npm-module/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ujs from './ujs';
+import ujs from './ujs'
 
 const CLASS_ATTRIBUTE_NAME = 'data-react-class'
 const PROPS_ATTRIBUTE_NAME = 'data-react-props'
@@ -10,15 +10,15 @@ const WebpackerReact = {
   registeredComponents: {},
   wrapForHMR: null,
 
-  render: function (node, component) {
-      var propsJson = node.getAttribute(PROPS_ATTRIBUTE_NAME)
-      var props = propsJson && JSON.parse(propsJson)
+  render(node, component) {
+    const propsJson = node.getAttribute(PROPS_ATTRIBUTE_NAME)
+    const props = propsJson && JSON.parse(propsJson)
 
-      let reactElement = React.createElement(component, props)
-      if (this.wrapForHMR) {
-        reactElement = this.wrapForHMR(reactElement)
-      }
-      ReactDOM.render(reactElement, node)
+    let reactElement = React.createElement(component, props)
+    if (this.wrapForHMR) {
+      reactElement = this.wrapForHMR(reactElement)
+    }
+    ReactDOM.render(reactElement, node)
   },
 
   renderOnHMR(component) {
@@ -43,7 +43,7 @@ const WebpackerReact = {
   },
 
   register(component, options) {
-    var name = component.name || (options !== undefined && options.as);
+    const name = component.name || (options !== undefined && options.as)
 
     if (!name) {
       throw "Could not determine component name. Probably it's a functional component. " +
@@ -61,44 +61,39 @@ const WebpackerReact = {
   },
 
   mountComponents() {
-    var registeredComponents = this.registeredComponents
-    var toMount = document.querySelectorAll('[' + CLASS_ATTRIBUTE_NAME + ']')
+    const registeredComponents = this.registeredComponents
+    const toMount = document.querySelectorAll(`[${CLASS_ATTRIBUTE_NAME}]`)
 
-    for (var i = 0; i < toMount.length; ++i) {
-      var node = toMount[i]
-      var className = node.getAttribute(CLASS_ATTRIBUTE_NAME)
-      var component = registeredComponents[className]
+    toMount.forEach((node) => {
+      const className = node.getAttribute(CLASS_ATTRIBUTE_NAME)
+      const component = registeredComponents[className]
 
       if (component) {
         this.render(node, component)
       } else {
-        console.error('webpacker-react: cant render a component that has not been registered: ' + className)
+        console.error(`webpacker-react: cant render a component that has not been registered: ${className}`)
       }
-    }
+    })
   },
 
   unmountComponents() {
-    var mounted = document.querySelectorAll('[' + CLASS_ATTRIBUTE_NAME + ']')
-
-    for (var i = 0; i < mounted.length; ++i) {
-      var node = mounted[i];
-      ReactDOM.unmountComponentAtNode(node);
-    }
+    const mounted = document.querySelectorAll(`[${CLASS_ATTRIBUTE_NAME}]`)
+    mounted.forEach(node => ReactDOM.unmountComponentAtNode(node))
   },
 
   addEventEventhandlers() {
-    if (this.eventsRegistered == true) {
-      console.warn("webpacker-react: events have already been registered");
-      return false;
+    if (this.eventsRegistered === true) {
+      console.warn('webpacker-react: events have already been registered')
+      return false
     }
 
-    ujs.setup(this.mountComponents.bind(this), this.unmountComponents.bind(this));
+    ujs.setup(this.mountComponents.bind(this), this.unmountComponents.bind(this))
 
-    this.eventsRegistered = true;
-    return true;
+    this.eventsRegistered = true
+    return true
   }
 }
 
-WebpackerReact.addEventEventhandlers();
+WebpackerReact.addEventEventhandlers()
 
 export default WebpackerReact
--- javascript/webpacker_react-npm-module/src/ujs.js
+++ javascript/webpacker_react-npm-module/src/ujs.js
@@ -1,85 +1,76 @@
-const ujs = {
-  handleEvent: function (eventName, callback, {once} = {once: false}) {
-    // jQuery is optional. Use it to support legacy browsers.
-    var $ = (typeof window.jQuery !== 'undefined') && window.jQuery;
+const $ = (typeof window.jQuery !== 'undefined') && window.jQuery
+const Turbolinks = window.Turbolinks
 
+const ujs = {
+
+  handleEvent(eventName, callback, { once } = { once: false }) {
     if ($) {
       if (once) {
-        $(document).one(eventName, callback);
+        $(document).one(eventName, callback)
       } else {
-        $(document).on(eventName, callback);
+        $(document).on(eventName, callback)
       }
     } else {
-      document.addEventListener(eventName, callback, {once});
+      document.addEventListener(eventName, callback, { once })
     }
   },
 
-  setup: function (onMount, onUnmount) {
-    var $ = (typeof window.jQuery !== 'undefined') && window.jQuery;
+  setup(onMount, onUnmount) {
     // Detect which kind of events to set up:
     if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
       if (typeof Turbolinks.EVENTS !== 'undefined') {
         // Turbolinks.EVENTS is in classic version 2.4.0+
-        this.turbolinksClassic(onMount, onUnmount);
-      } else if (typeof Turbolinks.controller !== "undefined") {
+        this.turbolinksClassic(onMount, onUnmount)
+      } else if (typeof Turbolinks.controller !== 'undefined') {
         // Turbolinks.controller is in version 5+
-        this.turbolinks5(onMount, onUnmount);
+        this.turbolinks5(onMount, onUnmount)
       } else {
-        // ReactRailsUJS.TurbolinksClassicDeprecated.setup();
-        this.turbolinksClassicDeprecated(onMount, onUnmount);
+        this.turbolinksClassicDeprecated(onMount, onUnmount)
       }
     } else if ($ && typeof $.pjax === 'function') {
-      this.pjax(onMount, onUnmount);
+      this.pjax(onMount, onUnmount)
     } else {
-      this.native(onMount);
+      this.native(onMount)
     }
   },
 
-  turbolinks5: function (onMount, onUnmount) {
-    this.handleEvent('turbolinks:load', onMount, {once: true});
-    this.handleEvent('turbolinks:render', onMount);
-    this.handleEvent('turbolinks:before-render', onUnmount);
+  turbolinks5(onMount, onUnmount) {
+    this.handleEvent('turbolinks:load', onMount, { once: true })
+    this.handleEvent('turbolinks:render', onMount)
+    this.handleEvent('turbolinks:before-render', onUnmount)
   },
 
-  turbolinksClassic: function (onMount, onUnmount) {
-    this.handleEvent(Turbolinks.EVENTS.CHANGE, onMount);
-    this.handleEvent(Turbolinks.EVENTS.BEFORE_UNLOAD, onUnmount);
+  turbolinksClassic(onMount, onUnmount) {
+    this.handleEvent(Turbolinks.EVENTS.CHANGE, onMount)
+    this.handleEvent(Turbolinks.EVENTS.BEFORE_UNLOAD, onUnmount)
   },
 
-  turbolinksClassicDeprecated: function (onMount, onUnmount) {
+  turbolinksClassicDeprecated(onMount, onUnmount) {
     // Before Turbolinks 2.4.0, Turbolinks didn't
     // have named events and didn't have a before-unload event.
     // Also, it didn't work with the Turbolinks cache, see
     // https://github.com/reactjs/react-rails/issues/87
-    Turbolinks.pagesCached(0);
-    this.handleEvent('page:change', onMount);
-    this.handleEvent('page:receive', onUnmount);
+    Turbolinks.pagesCached(0)
+    this.handleEvent('page:change', onMount)
+    this.handleEvent('page:receive', onUnmount)
   },
 
-  pjax: function (onMount, onUnmount) {
-    this.handleEvent('ready', onMount);
-    this.handleEvent('pjax:end', onMount);
-    this.handleEvent('pjax:beforeReplace', onUnmount);
+  pjax(onMount, onUnmount) {
+    this.handleEvent('ready', onMount)
+    this.handleEvent('pjax:end', onMount)
+    this.handleEvent('pjax:beforeReplace', onUnmount)
   },
 
-  native: function (onMount) {
-    var $ = (typeof window.jQuery !== 'undefined') && window.jQuery;
-
+  native(onMount) {
     if ($) {
-      $(function () {
-        onMount()
-      });
+      $(() => onMount())
     } else if ('addEventListener' in window) {
-      document.addEventListener('DOMContentLoaded', function () {
-        onMount()
-      });
+      document.addEventListener('DOMContentLoaded', onMount)
     } else {
       // add support to IE8 without jQuery
-      window.attachEvent('onload', function () {
-        onMount()
-      });
+      window.attachEvent('onload', onMount)
     }
   }
 }
 
-export default ujs;
+export default ujs

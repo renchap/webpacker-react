29173cd53923a5cace27a6e4c33c89405046505e
Renaud Chaput <renchap@gmail.com>
2022-08-16 12:46:47 +0200

    Switch to a proper class

--- src/index.ts
+++ src/index.ts
@@ -9,43 +9,56 @@ const PROPS_ATTRIBUTE_NAME = "data-react-props"
 
 declare global {
   interface Window {
-    ReactComponentsRails: typeof ReactComponentsRails
+    ReactComponentsRails: ReactComponentsRails
   }
 }
 
-const ReactComponentsRails = {
-  registeredComponents: {} as { [name: string]: React.ComponentType },
-  mountedRoots: [] as Root[],
-  ReactDOMClient: undefined as
+class ReactComponentsRails {
+  #registeredComponents = {} as { [name: string]: React.ComponentType }
+  #mountedRoots = [] as Root[]
+  #ReactDOMClient = undefined as
     | typeof ReactDOM
     | typeof ReactDOMClient
-    | undefined,
+    | undefined
 
-  render(node: Element, component: React.ComponentType) {
+  static getInstance() {
+    if (typeof window.ReactComponentsRails === "undefined") {
+      const instance = new ReactComponentsRails()
+      window.ReactComponentsRails = instance
+
+      // ujs.setup(this.mountComponents.bind(this), this.unmountComponents.bind(this))
+    }
+
+    return window.ReactComponentsRails
+  }
+
+  private render(node: Element, component: React.ComponentType) {
     const propsJson = node.getAttribute(PROPS_ATTRIBUTE_NAME)
     const props = propsJson && JSON.parse(propsJson)
 
     const reactElement = React.createElement(component, props)
 
-    if (this.ReactDOMClient) {
-      if ("createRoot" in this.ReactDOMClient) {
-        const root = this.ReactDOMClient.createRoot(node)
+    if (this.#ReactDOMClient) {
+      if ("createRoot" in this.#ReactDOMClient) {
+        const root = this.#ReactDOMClient.createRoot(node)
         root.render(reactElement)
-        this.mountedRoots.push(root)
+        this.#mountedRoots.push(root)
       } else {
-        this.ReactDOMClient.render(reactElement, node)
+        this.#ReactDOMClient.render(reactElement, node)
       }
     }
-  },
+  }
 
-  registerComponents(components: { [name: string]: React.ComponentType }) {
+  private registerComponents(components: {
+    [name: string]: React.ComponentType
+  }) {
     const alreadyExisting: string[] = []
 
     Object.keys(components).forEach((key) => {
-      if (this.registeredComponents[key]) alreadyExisting.push(key)
+      if (this.#registeredComponents[key]) alreadyExisting.push(key)
       else {
         const comp = components[key]
-        this.registeredComponents[key] = comp
+        this.#registeredComponents[key] = comp
       }
     })
 
@@ -58,24 +71,24 @@ const ReactComponentsRails = {
     }
 
     return true
-  },
+  }
 
-  unmountComponents() {
-    if (this.ReactDOMClient) {
-      if ("createRoot" in this.ReactDOMClient) {
-        this.mountedRoots.forEach((root) => root.unmount())
-        this.mountedRoots = []
-      } else {
-        const mounted = document.querySelectorAll(`[${CLASS_ATTRIBUTE_NAME}]`)
-        for (let i = 0; i < mounted.length; i += 1) {
-          this.ReactDOMClient.unmountComponentAtNode(mounted[i])
-        }
-      }
-    }
-  },
+  // Not used for now, useful for UJS
+  // private unmountComponents() {
+  //   if (this.#ReactDOMClient) {
+  //     if ("createRoot" in this.#ReactDOMClient) {
+  //       this.#mountedRoots.forEach((root) => root.unmount())
+  //       this.#mountedRoots = []
+  //     } else {
+  //       const mounted = document.querySelectorAll(`[${CLASS_ATTRIBUTE_NAME}]`)
+  //       for (let i = 0; i < mounted.length; i += 1) {
+  //         this.#ReactDOMClient.unmountComponentAtNode(mounted[i])
+  //       }
+  //     }
+  //   }
+  // }
 
-  mountComponents() {
-    const { registeredComponents } = this
+  private mountComponents() {
     const toMount = document.querySelectorAll(`[${CLASS_ATTRIBUTE_NAME}]`)
 
     for (let i = 0; i < toMount.length; i += 1) {
@@ -89,7 +102,7 @@ const ReactComponentsRails = {
         continue
       }
 
-      const component = registeredComponents[className]
+      const component = this.#registeredComponents[className]
 
       if (component) {
         if (node.innerHTML.length === 0) this.render(node, component)
@@ -99,34 +112,31 @@ const ReactComponentsRails = {
         )
       }
     }
-  },
+  }
 
   setup(components = {}) {
-    if (typeof window.ReactComponentsRails === "undefined") {
-      window.ReactComponentsRails = this
-      // ujs.setup(this.mountComponents.bind(this), this.unmountComponents.bind(this))
-    }
-
     this.loadReactDOMClient().then(() => {
       window.ReactComponentsRails.registerComponents(components)
       window.ReactComponentsRails.mountComponents()
     })
-  },
+  }
 
-  loadReactDOMClient() {
+  private loadReactDOMClient() {
     return new Promise<void>((resolve) => {
-      if (this.ReactDOMClient) resolve()
+      if (this.#ReactDOMClient) resolve()
 
       import("react-dom/client")
         .then((i) => {
-          this.ReactDOMClient = i
+          this.#ReactDOMClient = i
           resolve()
         })
         .catch(() => {
-          this.ReactDOMClient = ReactDOM
+          this.#ReactDOMClient = ReactDOM
         })
     })
-  },
+  }
 }
 
-export default ReactComponentsRails
+const instance = ReactComponentsRails.getInstance()
+
+export default instance
